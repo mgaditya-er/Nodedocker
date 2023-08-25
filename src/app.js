d8629a34d0e720f620f7034373e446aba985b6ff
mgaditya-er <aditya_ghyar@moderncoe.edu.in>
2023-08-26 03:02:09 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+var indexRouter = require('./routes/index');
+// add the following line near the indexRouter
+var personsRouter = require('./routes/persons');
+
+// app.js
+
+app.use('/', indexRouter);
+
+// add the following line near app.use indexRouter
+app.use('/persons', personsRouter);
+
+
+module.exports = app;
